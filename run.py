47cc534deee495b671754bd91cf0befee4bbb017
GitMulC <cmulvo@gmail.com>
2022-01-03 15:38:44 +0000

    Add intro print msg

--- run.py
+++ run.py
@@ -1,3 +1,2 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print('Welcome to GeoPolitik, the political game that puts you in command!\n'
+    'Face the geo-political challenges to become the dominant power!')
