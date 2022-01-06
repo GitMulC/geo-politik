98323c03c9e798e2d0e54a6423feb005a21381a4
GitMulC <cmulvo@gmail.com>
2022-01-06 18:31:31 +0000

    Add menu & input for selection

--- run.py
+++ run.py
@@ -1,2 +1,32 @@
-print('Welcome to GeoPolitik, the political game that puts you in command!\n')
-print('Face the geo-political challenges to become the dominant power!\n')
+def menu():
+    """
+    Runs opening menu screen on startup.
+    """
+    while True:
+        print('Welcome to GeoPolitik, the geo-political adventure game!')
+        print('Face geo-political challenges to survive as a nation.\n')
+        print('About - a')
+        print('Play - p')
+        print('Quit - q\n')
+
+        selection = input('Select an option:')
+
+        if menu_validate(selection):
+            print(f'You have chosen {selection}')
+            break
+    return selection
+
+
+def menu_validate(values):
+    """
+    Check if selection in menu is correct.
+    """
+    if len(values) == 1:
+        print('Correct text!')
+        return True
+    else:
+        print('Wrong text!')
+        return False
+
+
+menu()
