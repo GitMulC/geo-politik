5aa7d2ab93627e294204628e7a384cc5f0dc3444
GitMulC <cmulvo@gmail.com>
2022-01-09 16:26:33 +0000

    Add new code for selection

--- run.py
+++ run.py
@@ -9,28 +9,43 @@ def menu():
         print('Play - p')
         print('Quit - q\n')
 
-        selection = input('Select an option: ')
+        selection = input('Select an option: \n')
 
-        if menu_validate(selection):
-            print(f'You have chosen {selection}')
-            break
+        if selection == 'a':
+            print('About')
+            return False
+        elif selection == 'p':
+            print('Play')
+            return False
+        elif selection == 'q':
+            print('Quit')
+            return False
+        else:
+            print('Please select a viable option.')
+            return True
     return selection
 
+    #     if menu_validate(selection):
+    #         print(f'You have chosen {selection}')
+    #         break
+    # return selection
 
-def menu_validate(values):
-    """
-    Check if selection in menu is correct.
-    """
-    if len(values) == 1:
-        print('Correct text!')
-        return True
-    elif len(values) != 1:
-        print('Wrong text!')
-        return False
-    elif values.isalpha():
-        return True
-    else:
-        return False
+
+# def menu_validate(values):
+#     """
+#     Check if selection in menu is correct.
+#     """
+#     # if len(values) == 1:
+#     #     print('Correct text!')
+#     #     return True
+#     # elif len(values) != 1:
+#     #     print('Wrong text!')
+#     #     return False
+#     # elif values.isalpha():
+#     #     return True
+#     if
+#     else:
+#         return False
 
 
 menu()
