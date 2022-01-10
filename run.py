1767e6c3fb6bc1754b82c5dc1230aebc584af7ff
GitMulC <cmulvo@gmail.com>
2022-01-10 00:34:18 +0000

    Add new while loop for main game loop

--- run.py
+++ run.py
@@ -48,4 +48,31 @@ def menu():
 #         return False
 
 
-menu()
+# menu()
+
+current_scenario = 0
+
+def main_game():
+    """
+    """
+    scenario_texts = [
+        'China flyovers. Defence misniter urges retaliation.',
+        'Something else bad happens.\nWhat do?',
+        'blah'
+    ]
+    while True:
+        print(scenario_texts[current_scenario])
+        selection = input('\nWhat will you do?\n')
+        decision()
+
+
+def decision(current_scenario,selection):
+    if selection == 'y':
+        print('You retaliate, China responds')
+        game_over()
+    elif selection == 'n':
+        print('You do nothing')
+        current_scenario += 1
+    elif selection == 'd':
+        print('You deliberate with you cabinet')
+        current_scenario += 1
