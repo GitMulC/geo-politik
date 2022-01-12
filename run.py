61d235675a40dc6977c06d32ff74ffe26d7309eb
GitMulC <cmulvo@gmail.com>
2022-01-12 21:39:36 +0000

    Add more print statements & remove obselete comments

--- run.py
+++ run.py
@@ -59,6 +59,11 @@ def menu_option():
 
 
 def main_menu():
+    """
+    Main menu function.
+    Brings up the main menu or opening screen
+    that lets the player select from a range of options
+    """
     clear()
     print(
         'Welcome to GeoPolitik, the text based Geo-political game!\n'
@@ -71,7 +76,7 @@ def main_menu():
     )
     option = menu_option()
     if option == 'p':
-        first_decision()
+        game1_first_decision()
     # elif option == 'a':
     #     about_page()
     # else:
@@ -94,119 +99,216 @@ def make_decision():
     return decision
 
 
-def first_decision():
+def game1_first_decision():
     """
-    First decision function in Taiwan game.
+    First decision function in Taiwan game/game1.
     Determines outcome of the first decision the user makes.
     """
     clear()
-    print('China flys fighter jets over Taipei regularly')
-    print('Your defence minister urges you to retaliate.')
-    print('Will you follow this advice?\n')
+    print(
+        'On the 100th anniversary of the Chinese Communist Party\n'
+        'China announces its intent to regain lost territory.\n'
+        "This puts the island nation of Taiwan in the China's crosshairs.\n"
+        'China begins intimidating Taiwan by flying military jets\n'
+        'over Taiwan, to intimidate and wear Taiwan down militarily.\n'
+        'In response your defence minister urges you to respond with force.\n'
+        'Will you follow this advice?\n'
+        )
     choice = make_decision()
     if choice in ['d', 'n']:
-        print('You wait it out and see what happens')
-        time.sleep(2)
-        second_decision()
+        print(
+            'You refuse to use any force to resolve the issue\n'
+            'You wait it out and see what China plans to do next.\n'
+        )
+        time.sleep(5)
+        game1_second_decision()
     else:
+        print(
+            'You retaliate and shoot down a Chinese fighter jet over Taiwan.\n'
+            "China has found it's excuse to invade Taiwan and reclaim\n"
+            'what they see as their territory!\n'
+            'The small Taiwanese military is no match for the sheer size\n'
+            'of the Chinese army and Taiwan is quickly overrun!\n'
+        )
         time.sleep(2)
         print('Game Over!')
         # Game over function here
 
 
-def second_decision():
+def game1_second_decision():
     """
-    Second decision function in Taiwan game.
+    Second decision function in Taiwan game/game1.
     Determines outcome of the second decision the user makes.
     """
     clear()
-    print('China intensifies its flyovers and invades Kinmen Island')
-    print('Your foreign minister suggests you let China have the island')
-    print('What will you do?\n')
+    print(
+        'China intensifies its flyovers over Taipei. Realizing that Taiwan\n'
+        'will not respond with force, the Chinese government decides to\n'
+        'amass troops near Kinmen island in preparation for an invasion.\n'
+        'As Kinmen island is part of Taiwan, you immediately condemn this\n'
+        'act and draw attention to it on the international stage.\n'
+        'Your intelligence agency says an invasion is imminent and with\n'
+        'little to no military presence in Kinmen island, you know that it\n'
+        'be taken swiftly by the Chinese army. You know you cannot win.\n'
+        'You assemble your government and ask for options. Aggressive force\n'
+        'will only provoke the Chinese, so this is not an option.\n'
+        'Your foreign minister suggests you let China have the island to\n'
+        'descalate the situation and prevent a full scale war with China.\n'
+        'Do you agree with your foreign minister?\n'
+        )
     choice = make_decision()
     if choice in ['d', 'n']:
-        print('Correct!')
-        time.sleep(2)
         if choice == 'n':
-            fourth_decision()
+            print('You decide to meet the Chinese in open war.\n')
+            time.sleep(5)
+            game1_fourth_decision()
         else:
-            third_decision()
+            print('You weigh out your options and again, you wait it out.\n')
+            time.sleep(5)
+            game1_third_decision()
     else:
-        time.sleep(2)
+        print(
+            'You evacuate the island leaving it open for Chinese occupations\n'
+            'Sensing weakness, China invades Taiwan feeling confident\n'
+            'that Taiwan will be too slow to respond. With a lack of\n'
+            'international will to support it, Taiwan falls to China.\n'
+            )
+        time.sleep(5)
         print('Game Over!')
         # Game over function here
 
 
-def third_decision():
+def game1_third_decision():
     """
-    Third decision function in Taiwan game.
+    Third decision function in Taiwan game/game1.
     Determines outcome of the third decision the user makes.
     """
     clear()
-    print('You are reluctant to fight and hesitate')
-    print('Japan and Korea then join the fight on your side')
-    print('You lose the island but gain allies')
-    print('Your defence minister asks you to appeal to the USA')
-    print('Do you appeal to the USA?\n')
+    print(
+        'While deliberating your allies in the region\n'
+        'Japan and Korea assure you they will assist you should China invade\n'
+        'Kinmen island. However, soon after this declaration, China invades\n'
+        'Kinmen. Your local forces offer little resistance and the island is\n'
+        'quickly taken by China. Your allies tell you they need time to\n'
+        'mobilize and they recommend you ask the USA for assitance.\n'
+        'Your defence minister reitarates this and says Taiwan will not last\n'
+        'long if China invades the island of Taiwan.\n'
+        'Do you appeal to the USA?\n'
+        )
     choice = make_decision()
     if choice in ['y', 'd']:
-        print('You contact the US')
-        time.sleep(2)
         if choice == 'y':
-            fifth_decision()
+            print(
+                'You reach out to the US and request immediate\n'
+                'assitance.\n'
+            )
+            time.sleep(5)
+            game1_fifth_decision()
         else:
-            fourth_decision()
+            print(
+                'You mobilize your troops and begin fighting China.\n'
+                "You think that your coalition's willingness to act\n"
+                'against China will be enough to deter it from\n'
+                'invading Taiwan.\n'
+            )
+            time.sleep(5)
+            game1_fourth_decision()
     else:
-        time.sleep(2)
+        print(
+            'You refuse to ask for US assitance and confidently\n'
+            'face China on the battlefield. Upon hearing this Japan\n'
+            'and Korea abandon you knowing full well without the US,\n'
+            'defeat is assured. You quickly surrender and\n'
+            'concede to Chinas demands\n'
+        )
+        time.sleep(5)
         print('Game Over!')
         # Game over function here
 
 
-def fourth_decision():
+def game1_fourth_decision():
     """
     Fourth decision function in Taiwan game.
     Determines outcome of the fourth decision the user makes.
     """
     clear()
-    print('You fight China')
-    print("But the odds aren't in your favour")
-    print('PLA takes Kinmen Island')
-    print('Your vice president urges you to send for assitance from the USA')
-    print('Do you send a special envoy to the US?\n')
+    print(
+        'Your fight with China is in vain. You know you cannot win\n'
+        'short term or long term.\n'
+        'Your vice president urges you to ask the US for help.\n'
+        'Do you send a special envoy to the US?\n'
+    )
     choice = make_decision()
     if choice == 'y':
-        fifth_decision()
+        print(
+            'You contact the US who respond and assure you that they will\n'
+            'support Taiwan in this war.\n'
+        )
+        time.sleep(5)
+        game1_fifth_decision()
     else:
-        time.sleep(2)
+        print(
+            'You refuse to ask for US assitance and confidently\n'
+            'face China on the battlefield. Upon hearing this Japan\n'
+            'and Korea abandon you knowing full well without the US,\n'
+            'defeat is assured. You quickly surrender and\n'
+            'concede to Chinas demands\n'
+        )
+        time.sleep(5)
         print('Game Over!')
         # Game over function here
 
 
-def fifth_decision():
+def game1_fifth_decision():
     """
     Fifth decision function in Taiwan game.
     Determines outcome of the fifth decision the user makes.
     """
     clear()
-    print('US responds and promises to come to your defence')
-    print('But this will take time.')
-    print('Your defence minister says they have a plan to strike')
-    print('Bejing with a lethal airstrike to take out the Chairman')
-    print('Do you carry out this strike?\n')
+    print(
+        'The US assembles its Pacific battlegroup and makes its way towards\n'
+        'Taiwan. In the meantime China is keeping the pressure on. Daily\n'
+        'airstrikes hit Taiwanese citizens and many civilians are killed\n'
+        'The international community pivots to defending Taiwan and condemns\n'
+        'China for its hostility. But only th US is close enough to prevent\n'
+        "a fullscale invasion of Taiwan, but they won't arrive for 2 more\n"
+        'weeks. Your defence minister lays out a military response to catch\n'
+        'the Chinese off guard and stall them until the US arrives\n'
+        'The plan involves stealth jets to strike major militray targets\n'
+        'and the costal cities in mainland China. It also involves an\n'
+        'airstrike on Bejing itself to take out Chinese military command.\n'
+        'Your government is split 50:50 on whether to enact this plan.\n'
+        'Do you carry out this strike?\n'
+    )
     choice = make_decision()
     if choice in ['d', 'n']:
-        time.sleep(2)
+        print(
+            'You refuse to act aggressively. Your defence forces and allies\n'
+            'stall the Chinese long enough for the US to arrive and stave\n'
+            'and invasion of Taiwan! China capitulates and signs a peace\n'
+            'deal mediated by the US.\n'
+        )
+        time.sleep(5)
         print('You Win!')
     else:
-        time.sleep(2)
+        print(
+            'You decide to give China a taste of its own medicine.\n'
+            'The plan goes ahead. Only 50% of the targets are hit\n'
+            "And you've lost 60% of the men and aircraft you sent.\n"
+            'The strike on Bejing fails aswell and rallies the Chinese\n'
+            'public against you. They demand Taiwanese blood in return\n'
+            "China is outraged that it's leaders were targeted and\n"
+            'carries out a nuclear strike in retaliation\n'
+        )
+        time.sleep(5)
         print('Game Over!')
         # Game over function here
 
 
+play_again = 'yes'
 """
 Play again loop to keep user within the game.
 """
-play_again = 'yes'
 while play_again == 'yes' or play_again == 'y':
     main_menu()
     play_again = input('Do you want to play again? (yes or y to play again): ')
